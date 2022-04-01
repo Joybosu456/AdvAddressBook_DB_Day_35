f985d5230edfe840326089189a5a74c55188a13b
JOY <joybosu@gmail.com>
2022-04-01 22:21:18 +0530

    [joy] Count by city and State

--- AddressBookRepo.cs
+++ AddressBookRepo.cs
@@ -13,6 +13,8 @@ namespace AdvAddressBook_ADO.NET
         //Represents a connection to Sql Server Database
         SqlConnection connection = new SqlConnection(connectionString);
 
+        public SqlCommand SqlCommand { get; private set; }
+
         // Checks the connection.
         public void DataBaseConnection()
         {
@@ -24,10 +26,123 @@ namespace AdvAddressBook_ADO.NET
             }
             catch (Exception e)
             {
-                Console.WriteLine(e.StackTrace);
+                throw new Exception(e.Message);
             }
         }
 
+        // Adds the new contact to data base.
+        public bool addNewContactToDataBase(AddressBookModel addressBookModel)
+        {
+            try
+            {
+                using (this.connection)
+                {
+                    SqlCommand cmd = new SqlCommand("SpAddAddressBookDetails", this.connection);
+                    //setting command type as stored procedure
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@FirstName", addressBookModel.FirstName);
+                    cmd.Parameters.AddWithValue("@LastName", addressBookModel.LastName);
+                    cmd.Parameters.AddWithValue("@Address", addressBookModel.Address);
+                    cmd.Parameters.AddWithValue("@City", addressBookModel.City);
+                    cmd.Parameters.AddWithValue("@State", addressBookModel.State);
+                    cmd.Parameters.AddWithValue("@Zip", addressBookModel.Zip);
+                    cmd.Parameters.AddWithValue("@PhoneNumber", addressBookModel.PhoneNumber);
+                    cmd.Parameters.AddWithValue("@Email", addressBookModel.Email);
+                    cmd.Parameters.AddWithValue("@AddressBookName", addressBookModel.AddressBookName);
+                    cmd.Parameters.AddWithValue("@AddressBookType", addressBookModel.AddressBookType);
+                    this.connection.Open();
+                    //Return the number of rows updated
+                    var result = cmd.ExecuteNonQuery();
+                    this.connection.Close();
+                    if (result != 0)
+                    {
+                        return true;
+                    }
+                    return false;
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            finally
+            {
+                this.connection.Close();
+            }
+        }
+
+        // Edits the existing contact to data base.
+        public bool EditExiContactToDataBase(AddressBookModel addressBookModel, string firstName)
+        {
+            try
+            {
+                using (this.connection)
+                {
+                    string query = @"update AddressBook_Table set lastname=@LastName,address=@Address,city=@City,
+                    state=@State,zip=@Zip,phonenumber=@PhoneNumber,email=@Email,addressbookname=@AddressBookName,
+                    addressbooktype=@AddressBookType  where FirstName=@firstName";
+                    SqlCommand cmd = new SqlCommand(query, this.connection);
+                    cmd.Parameters.AddWithValue("@FirstName", firstName);
+                    cmd.Parameters.AddWithValue("@LastName", addressBookModel.LastName);
+                    cmd.Parameters.AddWithValue("@Address", addressBookModel.Address);
+                    cmd.Parameters.AddWithValue("@City", addressBookModel.City);
+                    cmd.Parameters.AddWithValue("@State", addressBookModel.State);
+                    cmd.Parameters.AddWithValue("@Zip", addressBookModel.Zip);
+                    cmd.Parameters.AddWithValue("@PhoneNumber", addressBookModel.PhoneNumber);
+                    cmd.Parameters.AddWithValue("@Email", addressBookModel.Email);
+                    cmd.Parameters.AddWithValue("@AddressBookName", addressBookModel.AddressBookName);
+                    cmd.Parameters.AddWithValue("@AddressBookType", addressBookModel.AddressBookType);
+                    this.connection.Open();
+                    var result = cmd.ExecuteNonQuery();
+                    this.connection.Close();
+                    if (result != 0)
+                    {
+                        return true;
+                    }
+                    return false;
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            finally
+            {
+                this.connection.Close();
+            }
+        }
+
+        // Deletes the existing contact in data base.
+        public bool deleteExiContactInDataBase(string firstName)
+        {
+            try
+            {
+                using (this.connection)
+                {
+                    SqlCommand cmd = new SqlCommand("SpAddAddressBookDetailsForDelete", this.connection);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@FirstName", firstName);
+                    this.connection.Open();
+                    var result = cmd.ExecuteNonQuery();
+                    this.connection.Close();
+                    if (result != 0)
+                    {
+                        return true;
+                    }
+                    return false;
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            finally
+            {
+                this.connection.Close();
+            }
+        }
+
+        // Person belonging to city or state.
         public void personBelongingCityOrState()
         {
             try
@@ -75,6 +190,50 @@ namespace AdvAddressBook_ADO.NET
             }
         }
 
-
+        // Count By City And State.
+        public void CountByCityAndState()
+        {
+            try
+            {
+                AddressBookModel addressBookModel = new AddressBookModel();
+                using (this.connection)
+                {
+                    using (SqlCommand command = new SqlCommand(
+                        @"select COUNT(*) as CityCount, City from AddressBook_Table group by City;
+                        select COUNT(*) as StateCount, State from AddressBook_Table group by State;; ", connection))
+                    {
+                        this.connection.Open();
+                        using (SqlDataReader sqlDataReader = command.ExecuteReader())
+                        {
+                            while (sqlDataReader.Read())
+                            {
+                                addressBookModel.City = sqlDataReader.GetString(0);
+                                int countCIty = sqlDataReader.GetInt32(1);
+                                Console.WriteLine("{0},{1}", addressBookModel.City, countCIty);
+                                Console.WriteLine("\n");
+                            }
+                            if (sqlDataReader.NextResult())
+                            {
+                                while (sqlDataReader.Read())
+                                {
+                                    addressBookModel.State = sqlDataReader.GetString(0);
+                                    int stateCount = sqlDataReader.GetInt32(1);
+                                    Console.WriteLine("{0},{1}", addressBookModel.State, stateCount);
+                                    Console.WriteLine("\n");
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            finally
+            {
+                this.connection.Close();
+            }
+        }
     }
 }
