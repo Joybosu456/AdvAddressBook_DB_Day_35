d686ac7eeac2b66ccadc401bfb59ee84559d057b
JOY <joybosu@gmail.com>
2022-04-01 21:57:51 +0530

    [Joy] Push AddressBook Repo Program file

---
+++ AddressBookRepo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace AdvAddressBook_ADO.NET
+{
+    public class AddressBookRepo
+    {
+        //Give path for Database Connection
+        public static string connectionString = @"Data Source=(Localdb)\MSSQLLocalDB;Initial Catalog=AddressBook_Service;Integrated Security=True";
+        //Represents a connection to Sql Server Database
+        SqlConnection connection = new SqlConnection(connectionString);
+
+        // Checks the connection.
+        public void DataBaseConnection()
+        {
+            try
+            {
+                this.connection.Open();
+                Console.WriteLine("connection established");
+                this.connection.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.StackTrace);
+            }
+        }
+
+
+    }
+}
