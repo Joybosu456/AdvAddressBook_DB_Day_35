c846dfb3595f18b5df1b3a163ece406a3f860f4c
JOY <joybosu@gmail.com>
2022-04-01 21:55:54 +0530

    [Joy] adding addressbook model Program file

---
+++ AddressBookModel.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdvAddressBook_ADO.NET
+{
+    public class AddressBookModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public Int64 Zip { get; set; }
+        public Int64 PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string AddressBookName { get; set; }
+        public string AddressBookType { get; set; }
+    }
+}
