6bfdde0daa04985e54a4a2c32f522b1b72a00631
JOY <joybosu@gmail.com>
2022-04-01 22:03:48 +0530

    [Joy] Push Ability to create Person details

--- AddressBookRepo.cs
+++ AddressBookRepo.cs
@@ -28,6 +28,45 @@ namespace AdvAddressBook_ADO.NET
             }
         }
 
+        public bool addNewContactToDataBase(AddressBookModel addressBookModel)
+        {
+            try
+            {
+                using (this.connection)
+                {
+                    SqlCommand cmd = new SqlCommand("SpAddAddressBookDetails", this.connection);
+                    //setting command type as stored procedure
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@FirstName", addressBookModel.FirstName);
+                    cmd.Parameters.AddWithValue("@LastName", addressBookModel.LastName);
+                    cmd.Parameters.AddWithValue("@Address", addressBookModel.Address);
+                    cmd.Parameters.AddWithValue("@City", addressBookModel.City);
+                    cmd.Parameters.AddWithValue("@State", addressBookModel.State);
+                    cmd.Parameters.AddWithValue("@Zip", addressBookModel.Zip);
+                    cmd.Parameters.AddWithValue("@PhoneNumber", addressBookModel.PhoneNumber);
+                    cmd.Parameters.AddWithValue("@Email", addressBookModel.Email);
+                    cmd.Parameters.AddWithValue("@AddressBookName", addressBookModel.AddressBookName);
+                    cmd.Parameters.AddWithValue("@AddressBookType", addressBookModel.AddressBookType);
+                    this.connection.Open();
+                    //Return the number of rows updated
+                    var result = cmd.ExecuteNonQuery();
+                    this.connection.Close();
+                    if (result != 0)
+                    {
+                        return true;
+                    }
+                    return false;
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            finally
+            {
+                this.connection.Close();
+            }
+        }
 
     }
 }
