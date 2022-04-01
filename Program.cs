ba05b26a84ebda513b179da5b96c229eb7dc7e7c
JOY <joybosu@gmail.com>
2022-04-01 21:35:53 +0530

    [Joy] Welcome to my DB address book program

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace AdvAddressBook_ADO.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome TO AddressBook Problems Using ADO.NET");
+
+            //AddressBookRepo addressBookRepo = new AddressBookRepo();
+            //AddressBookModel addressBookModel = new AddressBookModel();
+            //addressBookModel.FirstName = "Diksha";
+            //addressBookModel.LastName = "Budde";
+            //addressBookModel.Address = "Vakil Colony";
+            //addressBookModel.City = "latur";
+            //addressBookModel.State = "Maharashtra";
+            //addressBookModel.Zip = 413512;
+            //addressBookModel.PhoneNumber = 8149713160;
+            //addressBookModel.Email = "poonam@gmail.com";
+            //addressBookModel.AddressBookName = "friend address book";
+            //addressBookModel.AddressBookType = "Friend";
+            //addressBookRepo.DataBaseConnection();
+            //addressBookRepo.addNewContactToDataBase(addressBookModel);
+            //addressBookRepo.EditExiContactToDataBase(addressBookModel, "poonam");
+            //addressBookRepo.deleteExiContactInDataBase("Diksha");
+            //addressBookRepo.personBelongingCityOrState();
+        }
+    }
+}
